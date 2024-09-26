d54acfa58172d64fd1f5037d7c7ef28a839e82be
Livia <161737244+LiviacoelhoM@users.noreply.github.com>
2024-09-26 09:25:30 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,9 @@
+//aventura, comédia, fantasia, ação, terror, drama, suspense  
+
+//a dama e o vagabundo, 10, aventura, comédia
+//meu malvado favorito, LIVRE, comédia, fantasia
+//Cluella, 12, aventura, comédia
+//a princesa e o sapo, LIVRE, comédia 
+//Teen Wolf:The Movie, 14, ação, fantasia, terror 
+//elite, 18, drama,suspense
+
